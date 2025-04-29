44f06b15b522e685969c7c8c443990276681a25e
Felix Sowah <60888274+Lancelot-SO@users.noreply.github.com>
2025-04-29 13:07:58 +0000

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -33,11 +33,11 @@ const Hero = () => {
               md:text-5xl md:leading-tight lg:text-6xl
               lg:leading-[1.2] font-500 md:tracking-[-2px]"
             >
-              I am a UI/UX Designer &
+              I am a Full-stack Web Developer &
               {' '}
               <br />
               {' '}
-              Full-stack Web Developer
+              UI/UX Designer
             </h2>
             <p className="pt-4 pb-8 md:pt-6 md:pb-12
               max-w-[480px] text-lg texxt-center
